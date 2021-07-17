1de512d44d0cbc03e9d4c621ccc9d480e69686e8
Mariana Amina <mariana.dantasweb7@gmail.com>
2021-07-17 18:28:51 -0300

    Make the users able to put their markers on gameBoard

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,86 @@
+/* 
+    Pseudocode
+
+
+
+*/
+
 /* DOM VARIABLES */
 const gameSpots = document.querySelectorAll(".gameSpot");
 
 
 
 const playerFactory = (number, mark) => {
-    if (number === 1 || number === 2) {
-        if (mark === "O" || mark === "X") return {number, mark};
-        else return "Invalid Mark";
+    if ((number === 1 || number === 2) && (mark === "O" || mark === "X")) {
+        const getNumber = () => number;
+
+        const getMark = () => mark;
+
+        return {getNumber, getMark};
     }
-    else return "Invalid Number";
+    else return "Invalid Number or Mark";
 };
 
 const gameBoard = function(){
-    const gameboardArray = ["X", "X", "X", "O", "O", "O", "X", "X", "X"];
-    
-    return {
+    "use strict";
 
+    const gameboardArray = ["", "", "", "", "", "", "", "", ""];
+
+    const playRecorder = [];
+
+    return {
+        gameboardArray,
+        playRecorder
     }
 }();
 
 const gameController = function(){
+    "use strict";
 
+    let player1 = "";
+
+    let player2 = "";
+    
+    function definePlayers(){
+        alert("Welcome to the Amina's Tic Tac Toe!")
+        let markConfirm = confirm("Player 1 would like to play with the mark 'X'?");
+        if(markConfirm){
+            player1 = playerFactory(1, "X");
+            player2 = playerFactory(2, "O");
+        } else {
+            player1 = playerFactory(1, "O");
+            player2 = playerFactory(2, "X");
+        }
+    }
+
+    function startGame(event){
+        let verifyArray = 0;
+        for(let i = 0; i < gameBoard.gameboardArray.length; i++){
+            if(gameBoard.gameboardArray[i]){
+                verifyArray++;
+                if(verifyArray === 9) return;
+            }
+        }
+
+        if( gameBoard.playRecorder[gameBoard.playRecorder.length-1] === player2.getMark() ||
+            gameBoard.playRecorder[gameBoard.playRecorder.length-1] === undefined){
+            gameBoard.gameboardArray[event.currentTarget.id] = player1.getMark();
+            gameBoard.playRecorder.push(player1.getMark());
+        } else {
+            gameBoard.gameboardArray[event.currentTarget.id] = player2.getMark();
+            gameBoard.playRecorder.push(player2.getMark());
+        }
+
+        event.currentTarget.textContent = gameBoard.gameboardArray[event.currentTarget.id];
+        console.log(gameBoard.gameboardArray);
+    }
+
+    return {
+        definePlayers,
+        startGame
+    }
 }();
 
-gameSpots.forEach(gameSpot => {
-    gameSpot.addEventListener("click", function(){
-        alert("Clicked");
-    })
-})
+gameController.definePlayers();
+
+gameSpots.forEach(gameSpot => {gameSpot.addEventListener("click", gameController.startGame)})
